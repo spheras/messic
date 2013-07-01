70ab5161d57dafceedefa83c42be837bc052f3ba
hansuber <manuel.hans.uber@gmail.com>
2013-07-01 16:37:01 +0200

    Login window

--- src/messic-server-webapp/src/main/webapp/js/login.js
+++ src/messic-server-webapp/src/main/webapp/js/login.js
@@ -5,11 +5,37 @@ $(document).ready(function() {
 		alert('closing!');
 	}
 
-	if (!window.data("kendoWindow")) {
-		window.kendoWindow({
-			width: "600px",
-			title: "About Alvar Aalto",
-			close: onClose
-		});
-	}
+	var win = $("#window").kendoWindow({
+		width: "600px",
+		title: "About Alvar Aalto",
+		close: onClose
+	}).data("kendoWindow").center();
+    		
 });
+
+function on_login()
+{
+	info = $("#login_form").serialize();
+	console.log(info);
+	$.ajax({
+        type: "POST",
+        url: "j_spring_security_check",
+        data: info, // serializes the form's elements.
+        dataType: "json",
+        error: function (XMLHttpRequest, textStatus, errorThrown) 
+        	{
+            	console.log('error');
+        	},
+        success: function (data) 
+        	{
+	        	if(data.success==true)
+	        	{
+	        		alert('ok');            	
+	        	}
+	        	else
+	        	{
+	        		$("#window").parent().effect("shake");
+	        	}
+        	}
+      });
+}
