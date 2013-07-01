f0dda0ca5d615841e63fc9578deec41538fc8b95
hansuber <manuel.hans.uber@gmail.com>
2013-07-01 16:44:38 +0200

    Business Layer example

--- src/messic-server-logics/src/main/java/org/messic/server/business/BUser.java
+++ src/messic-server-logics/src/main/java/org/messic/server/business/BUser.java
@@ -1,13 +1,25 @@
 package org.messic.server.business;
 
+import org.messic.server.datamodel.MDOUser;
 import org.messic.server.datamodel.dao.DAOUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
+@Component
 public class BUser {
 
 	@Autowired
     private DAOUser userRepository;
+	
+	public MDOUser getUser(String user)	{
+		return userRepository.getUser(user);
+	}
+	
+	public MDOUser createUser(String login, String password, String name,
+			boolean administrator) {
+		return userRepository.createUser(login, password, name, administrator);
+	}
 	
 }
