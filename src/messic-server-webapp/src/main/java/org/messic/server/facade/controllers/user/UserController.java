89d4713510d0c638cf7412656703ee1f1c7dcc02
mhu <manuel.hans.uber@gmail.com>
2013-12-27 19:35:19 +0100

    Delete import warnings.

--- src/messic-server-webapp/src/main/java/org/messic/server/facade/controllers/user/UserController.java
+++ src/messic-server-webapp/src/main/java/org/messic/server/facade/controllers/user/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
