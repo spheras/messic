1e46ae4fd706c0d13310ef6996af9d7c4d3e7925
mhu <manuel.hans.uber@gmail.com>
2013-12-27 19:45:16 +0100

    User management resources.

--- src/messic-server-datamodel/src/main/java/org/messic/server/datamodel/dao/DAOUser.java
+++ src/messic-server-datamodel/src/main/java/org/messic/server/datamodel/dao/DAOUser.java
@@ -10,15 +10,17 @@ public interface DAOUser extends DAO<MDOUser>
 {
     public MDOUser getUser(String user);
     
-    public MDOUser createUser(String login, String password, String name, boolean administrator);
+    public MDOUser createUser(String login, String password, String name, String email, byte[] avatar, boolean administrator);
     
     public MDOUser updatePassword(Long userSid, String newPassword);
     
-    public MDOUser updateUserData(Long userSid, String name);
+    public MDOUser updateUserData(Long userSid, String name, String email, byte[] avatar);
     
     public void removeUser(Long userSid);
     
     public boolean existUser(String login);
+    
+    public boolean existUsers();
 
     public MDOUser authenticate(String login, String password);
    
