6e83ffb34fd02cfd4bde4d7b8c2e57fc51a99093
spheras <joseamuedo@gmail.com>
2014-08-25 20:29:00 +0200

    web improvements

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+function loadAPI(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $.ajax({
+        url: "api.html",
+        success: function (data) {
+            $content = $("#content-wrapper");
+            $content.empty();
+            $content.append(data);
+
+            $(".messic-header2").addClass("messic-header2-min");
+        },
+    });
+}
