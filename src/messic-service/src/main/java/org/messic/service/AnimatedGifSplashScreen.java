5ac3d41499eaa1a47584789b9c4fc18e70dc897c
spheras <joseamuedo@gmail.com>
2014-09-07 01:32:51 +0200

    enhacement 65, splash screen improved. Loading dynamically animated gifs from the internet.

--- src/messic-service/src/main/java/org/messic/service/AnimatedGifSplashScreen.java
+++ src/messic-service/src/main/java/org/messic/service/AnimatedGifSplashScreen.java
@@ -9,9 +9,16 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
 import java.awt.RenderingHints;
+import java.awt.Toolkit;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.SocketAddress;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
@@ -21,11 +28,32 @@ import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 
 import org.apache.log4j.Logger;
+import org.messic.configuration.MessicConfig;
 
 public class AnimatedGifSplashScreen
     extends JWindow
 {
     private static Logger log = Logger.getLogger( AnimatedGifSplashScreen.class );
+
+    // List of known url of animated gifs at the web to show as splash screens
+    private static String[] knownGifUrls = { "http://33.media.tumblr.com/tumblr_lk23bhrYsh1qchrblo1_500.gif",
+        "http://38.media.tumblr.com/c6fded3abd6535f6b4f9cf7a73cea710/tumblr_mhnavyed481rkbqvco1_500.gif",
+        "http://33.media.tumblr.com/883e4eeb283c668482a9f57d49ca5202/tumblr_mir5epOXxe1rmgj27o1_500.gif",
+        "http://38.media.tumblr.com/6d2d7ed3eb81a5f1971a56fe22a4fafc/tumblr_n4fmibUE3m1qexuy0o1_500.gif",
+        "http://31.media.tumblr.com/c5cbbdc568b552b910fe3c19d22bf4ae/tumblr_n6zxafDJC81tnzwcoo1_500.gif",
+        "http://38.media.tumblr.com/3e61bf2940872bf9998bec29f923f377/tumblr_n6ni53w3Ou1tbb7cno1_500.gif",
+        "http://33.media.tumblr.com/bfbda61e7201c1266de2ce01666ea6bd/tumblr_mtw7g5Bfcu1sg7iqko1_400.gif",
+        "http://media.giphy.com/media/14mssh7YCCk61G/giphy.gif",
+        "www.dj-rooms.com/wp-content/uploads/2011/08/djrooms_anim05.gif",
+        "http://www.dj-rooms.com/wp-content/uploads/2011/08/djrooms_anim06.gif",
+        "http://www.dj-rooms.com/wp-content/uploads/2011/08/djrooms_anim07.gif",
+        "http://www.dj-rooms.com/wp-content/uploads/2011/08/djrooms_anim08.gif",
+        "http://www.dj-rooms.com/wp-content/uploads/2011/08/djrooms_anim10.gif",
+        "http://www.dj-rooms.com/wp-content/uploads/2011/08/djrooms_anim12.gif",
+        "http://www.dj-rooms.com/wp-content/uploads/2011/08/djrooms_anim13.gif",
+        "http://www.dj-rooms.com/wp-content/uploads/2011/08/djrooms_anim14.gif",
+        "http://www.dj-rooms.com/wp-content/uploads/2011/08/djrooms_anim01.gif",
+        "http://25.media.tumblr.com/027d6732a0153e38628445b394f07898/tumblr_n24lc8Wmhg1qk57pio1_500.gif" };
 
     /**
 	 * 
@@ -54,6 +82,82 @@ public class AnimatedGifSplashScreen
         return baos.toByteArray();
     }
 
+    /**
+     * http://stackoverflow.com/questions/363681/generating-random-integers-in-a-range-with-java Returns a pseudo-random
+     * number between min and max, inclusive. The difference between min and max can be at most
+     * <code>Integer.MAX_VALUE - 1</code>.
+     * 
+     * @param min Minimum value
+     * @param max Maximum value. Must be greater than min.
+     * @return Integer between min and max, inclusive.
+     * @see java.util.Random#nextInt(int)
+     */
+    public static int randInt( int min, int max )
+    {
+
+        // NOTE: Usually this should be a field rather than a method
+        // variable so that it is not re-seeded every call.
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt( ( max - min ) + 1 ) + min;
+
+        return randomNum;
+    }
+
+    private void getRandomImageIcon( final JLabel label )
+        throws IOException
+    {
+        Thread t = new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    URL url = new URL( knownGifUrls[randInt( 0, knownGifUrls.length - 1 )] );
+                    Proxy proxy = getProxy();
+                    URLConnection uc;
+                    if ( proxy != null )
+                    {
+                        uc = url.openConnection( proxy );
+                    }
+                    else
+                    {
+                        uc = url.openConnection();
+                    }
+                    ImageIcon ii = new ImageIcon( readInputStream( uc.getInputStream() ) );
+                    label.setIcon( ii );
+                    setSize( ii.getIconWidth() + 20, ii.getIconHeight() + 20 );
+                    setBackground( new Color( 0x444444 ) );
+                    getContentPane().setBackground( Color.LIGHT_GRAY );
+                    Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+                    setLocation( dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2 );
+                }
+                catch ( Exception e )
+                {
+                    e.printStackTrace();
+                }
+            };
+        };
+        t.start();
+    }
+
+    private Proxy getProxy()
+    {
+        MessicConfig mc = new MessicConfig();
+
+        String url = (String) mc.getProxyUrl();
+        String port = (String) mc.getProxyPort();
+        if ( url != null && port != null && url.length() > 0 && port.length() > 0 )
+        {
+            SocketAddress addr = new InetSocketAddress( url, Integer.valueOf( port ) );
+            Proxy proxy = new Proxy( Proxy.Type.HTTP, addr );
+            return proxy;
+        }
+        return null;
+    }
+
     public AnimatedGifSplashScreen()
         throws IOException
     {
@@ -61,12 +165,11 @@ public class AnimatedGifSplashScreen
 
         label.setHorizontalAlignment( SwingConstants.CENTER );
 
-        ImageIcon ii =
-            new ImageIcon(
-                           readInputStream( this.getClass().getResourceAsStream( "/tumblr_n24lc8Wmhg1qk57pio1_500.gif" ) ) );
-        label.setIcon( ii );
+        getRandomImageIcon( label );
+        getContentPane().setBackground( new Color( 0x000000 ) );
         add( label, BorderLayout.CENTER );
         label.setBorder( new LineBorder( new Color( 0x888888 ), 3 ) );
+        label.setBackground( new Color( 0x000000 ) );
 
         // Set up the glass pane, which appears over both menu bar
         // and content pane and is an item listener on the change
@@ -76,13 +179,14 @@ public class AnimatedGifSplashScreen
         myGlassPane.setVisible( true );
 
         // frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        setMinimumSize( new Dimension( 360, 220 ) );
-        setSize( ii.getIconWidth() + 20, ii.getIconHeight() + 20 );
-        setBackground( new Color( 0x444444 ) );
+        // setMinimumSize( new Dimension( 360, 220 ) );
+        setSize( 400, 100 );
+
+        setBackground( new Color( 0x000000 ) );
         // frame.pack();
         setLocationRelativeTo( null );
         setVisible( true );
-        setAlwaysOnTop( true );
+        // setAlwaysOnTop( true );
     }
 
     /**
